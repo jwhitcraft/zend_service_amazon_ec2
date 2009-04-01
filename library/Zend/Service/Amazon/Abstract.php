115ebda9a94a684d2333696aef0fa8aeccf1e488
Sid Highwind <sidhighwind@peter.(none)>
2009-04-01 19:45:41 -0400

    Added Zend Doc Blocks to each file

--- library/Zend/Service/Amazon/Abstract.php
+++ library/Zend/Service/Amazon/Abstract.php
@@ -1,7 +1,37 @@
 <?php
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   Zend
+ * @package    Zend_Service
+ * @subpackage Amazon
+ * @copyright  Copyright (c) 2005-2009 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ * @version    $Id$
+ */
 
 require_once 'Zend/Service/Abstract.php';
 
+/**
+ * Abstract Amazon class that handles the credentials for any of the Web Services that
+ * Amazon offers
+ *
+ * @category   Zend
+ * @package    Zend_Service
+ * @subpackage Amazon
+ * @copyright  Copyright (c) 22005-2009 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
 abstract class Zend_Service_Amazon_Abstract extends Zend_Service_Abstract
 {
     /**
